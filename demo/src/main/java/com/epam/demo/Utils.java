9b2e441a3e7891fbec1a52b7bc47720f23571d32
wezelbul <d.chesnokov.01@yandex.ru>
2023-08-01 01:48:46 +0300

    feat: solution

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,6 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        return args.stream().allMatch(str -> Integer.parseInt(str) > 0);
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,6 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+         return Integer.parseInt(str) > 0;
     }
 }
